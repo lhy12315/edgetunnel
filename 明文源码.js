33bc731536b19a81bd36bf71f96f1d06eddc1d26
lhy12315 <46898773+lhy12315@users.noreply.github.com>
2024-12-22 01:09:50 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1288,7 +1288,7 @@ async function 生成配置信息(userID, hostName, sub, UA, RproxyIP, _url, env
 	
 				return randomIP.join('.');
 			}
-			addresses = addresses.concat('127.0.0.1:1234#CFnat');
+			addresses = addresses.concat('192.168.2.249:1234#CFnat');
 			if (hostName.includes(".workers.dev")) {
 				addressesnotls = addressesnotls.concat(cfips.map(cidr => generateRandomIPFromCIDR(cidr) + '#CF随机节点'));
 			} else {
